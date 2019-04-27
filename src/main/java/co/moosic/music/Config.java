1f41ad036191896e26faa6170727b295841a8c04
RootAndroid58 <rootand58@gmail.com>
2019-04-27 19:33:28 +0530

    Update Config.java

--- src/main/java/co/moosic/music/Config.java
+++ src/main/java/co/moosic/music/Config.java
@@ -4,14 +4,14 @@ import com.kaaz.configuration.ConfigurationOption;
 
 public class Config {
     @ConfigurationOption
-    public static String discord_token = "your-discord-token";
+    public static String discord_token = "NTcxNjkwODMxMDAyNzMwNTE2.XMRaww.1ErdM67EFXSSdzlHxfJ_GdCBdh0";
 
     @ConfigurationOption
-    public static String command_prefix = "!";
+    public static String command_prefix = "!!";
 
     @ConfigurationOption
     public static int volume = 30;
 
     @ConfigurationOption
-    public static String voice_channel_id = "channel-id";
+    public static String voice_channel_id = "571685400746131486";
 }
